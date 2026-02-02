12d0be5b4052dba14aaedd0933ccd576a7989203
KarChikey <kartikeynegi2002@gmail.com>
2026-02-02 20:39:13 +0530

    add

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+import express from 'express'
+import cors from 'cors'
+const mainrouter=require("./routes/index.js");
+
+const app=express()
+app.use(cors())
+app.use(express.json())
+app.use("/api/v1",mainrouter)
