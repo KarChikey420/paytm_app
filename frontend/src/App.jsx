933964e1cc288b4a75cbf326660e53a6fb264bde
KarChikey <kartikeynegi2002@gmail.com>
2026-02-05 00:14:06 +0530

    add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Appbar } from './component/Appbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    
+     <Appbar/>
     </>
   )
 }
