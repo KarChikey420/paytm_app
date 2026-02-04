365967f2a8958efbdbc258b2eb0e244b49a8ed80
KarChikey <kartikeynegi2002@gmail.com>
2026-02-05 00:35:32 +0530

    add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
-import { Appbar } from './component/Appbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <Appbar/>
+     
     </>
   )
 }
